7f74a71ba82881f48523b13f696905ee5cdc5b72
Daniel Lee <daniel@dotmodus.com>
2020-12-06 12:04:32 +0000

    :sparkles: Some set theory makes this an easy one
    - part a: set.add()
    - part b: set.intersection()
    
    Actually had a bit of an issue with the end of the input file
    being a newline. Odd by 1 error.

---
+++ 2021/6.py
@@ -0,0 +1,34 @@
+def solve_a(l: list) -> int:
+    """Count the total number of people that answered yes"""
+    total = 0
+    for group in l:
+        questions = set()
+        for person in group.split("\n"):
+            for question in person:
+                questions.add(question)
+        total += len(questions)
+
+    return total
+
+
+def solve_b(l: list) -> int:
+    """Count the groups that answered yes"""
+    total = 0
+    for group in l:
+        q = set()
+        for i, person in enumerate(group.split('\n')):
+            if i == 0:
+                q = set(person)
+            else:
+                q = q.intersection(set(person))
+        total += len(q)
+
+    return total
+
+
+if __name__ == '__main__':
+    with open("6.txt") as f:
+        input = f.read().split("\n\n")
+
+    print(solve_a(input))
+    print(solve_b(input))
