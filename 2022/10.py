c5837c90abacdaf62c4ff11d0c9b01f4ea4b23ca
Daniel Lee <daniel@dotmodus.com>
2022-12-14 08:37:26 +0200

    :sparkles: part 2 done

--- 2022/10.py
+++ 2022/10.py
@@ -23,8 +23,42 @@ def solve_a(rows):
     return total
 
 
+def solve_b(rows):
+    output = ""
+    q = deque()
+    x = 1
+    i = 1
+    pixel = 0
+    while i < 220:
+        for row in rows:
+            if row[0] == 'a':
+                q.appendleft(int(row.split()[1]))
+                q.appendleft(0)
+            else:
+                q.appendleft(0)
+
+            if pixel in {x - 1, x, x + 1}:
+                output += '#'
+            else:
+                output += '.'
+            if i % 40 == 0:
+                pixel = -1
+                # return output
+                output += '\n'
+            if len(q) > 2:
+                x += q.pop()
+            i += 1
+            pixel += 1
+
+            if i > 240:
+                break
+
+    return output
+
+
 
 
 if __name__ == '__main__':
     rows = open("10.txt").read().split('\n')
     print(solve_a(rows))
+    print(solve_b(rows))
