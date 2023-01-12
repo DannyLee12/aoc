569fa4db0b3163f91b1c0bc28422942fb66e932d
Daniel Lee <daniel@dotmodus.com>
2023-01-12 14:21:31 +0200

    :sparkles: LinkedList implementation
    - proud of this, even if it did take me a long time

--- 2022/20.py
+++ 2022/20.py
@@ -10,44 +10,66 @@ class Node:
     def __repr__(self):
         return str(self.val)
 
+    def find(self, val):
+        node = self
+        while node.val != val:
+            node = node.next
+        return node
+
     def move(self, length):
         i = self.val % (length - 1)
         node = self
+        prev = self.prev
+
         while i:
+            # prev
+            next_node = node.next
+            prev.next = next_node
+            next_node.prev = prev
+            # curr
+            node = next_node
+            next_node = node.next
+            node.next = self
+            self.prev = node
+            # next
+            self.next = next_node
+            next_node.prev = self
+
+            prev = node
             node = node.next
             i -= 1
 
-        prev = self.prev
-        prev.next = self.next
-        self.prev = node
-        self.next = node.next
 
 if __name__ == '__main__':
     input = list(map(int,open("20.txt").read().split('\n')))
     n = len(input)
-    head = Node(input[0])
+    head = Node(input[0] * 811589153)
     node = head
     nodes = []
     for i, val in enumerate(input):
         if i == 0:
             continue
-        new_node = Node(val)
+        new_node = Node(val * 811589153)
         new_node.prev = node
         node.next = new_node
         nodes.append(node)
         node = node.next
 
+    nodes.append(node)
+
     node.next = head
     head.prev = node
 
-    for node in nodes:
-        head = node
+    for i in range(10):
+        print("Mix", i)
+        for node in nodes:
+            node.move(n)
 
-        val = ""
-        for _ in range(n):
-            val += str(head.val) + ", "
-            head = head.next
+    node = node.find(0)
+    total = 0
+    for _ in range(3):
+        for _ in range(1000 % n):
+            node = node.next
+        total += node.val
 
-        print(val)
-
-        node.move(n)
+    print(total)
